f96cfcbd3ce24cf24fbbb37b65642c25613e1af2
LarryGF <larrywtf609@gmail.com>
2018-02-24 21:25:25 -0500

    Add files via upload

---
+++ readingdb.py
@@ -0,0 +1,75 @@
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# engine = create_engine('sqlite:///test.db', echo=False)
+engine = create_engine('mysql://root:1234@localhost/zabbix', echo=False)
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Session = sessionmaker(bind=engine, expire_on_commit=False)
+session = Session()
+
+# File = Base.classes.files
+# a = session.query(File).filter(File.filename.like('%snake%')).all()
+# print([(e.path_parent + e.filename) for e in a])
+# ilike no es case sensitive
+hosts = Base.classes['hosts']
+items = Base.classes['items']
+trends = Base.classes['trends']
+
+
+def search_host(hostname: str):
+    """
+    El método recive un hostname y devuelve
+
+    (hosts.id, hosts.name)
+
+    donde hostname es un substring de hosts.name
+    """
+    return session.query(hosts.hostid, hosts.name).filter(hosts.name.ilike('%{}%'.format(hostname))).all()
+
+
+def get_items(hostid: int):
+    """
+    El método recive un hostid y devuelve
+
+    (items.hostid, items.itemid, items.name)
+
+    donde items.hostid == hostid
+    """
+    return session.query(items.hostid, items.itemid, items.name).filter(items.hostid == hostid).all()
+
+
+def get_trends(itemid: int):
+    """
+    El método recive un itemid y devuelve
+
+    (trends.itemid, trends.value_min, trends.value_avg, trends.value_max)
+
+    donde trends. itemid == hostid
+    """
+    return session.query(trends.itemid, trends.value_min, trends.value_avg, trends.value_max).filter(trends.itemid == itemid).all()
+
+
+def percentil(scores):
+    count = 0
+    ans = []
+    for i, score in enumerate(scores):
+        i+=1
+
+        ans.append(i/len(scores))
+
+    return ans
+
+
+def rank(scores, percentil_rank, value):
+    
+
+    for i, e in enumerate(percentil_rank):
+        if e >= value:
+            return scores[i]
+
+def mean(numbers):
+	ans = []     
+	mean = float( sum(numbers)) / max(len(numbers),1)
+	return mean
---
+++ zabbixInfo.py
@@ -0,0 +1,108 @@
+
+import click
+from readingdb import get_items, get_trends, search_host, percentil, rank, mean
+
+@click.command()
+@click.argument('name')
+def hosts(name):
+    host_list = search_host(name)
+    hosts_ids = [e[0] for e in host_list]
+    select = ''
+
+    click.echo('Hostid\tHostname')
+    for host in host_list:
+        print('{}\t{}'.format(*host))
+    while not select.isnumeric() or  int(select) not in hosts_ids:
+        select = click.prompt('\nSelect a hostid')
+
+        if not select.isnumeric() or int(select) not in hosts_ids:
+            click.echo('\nPlease use a number from the list')
+
+    click.echo('You selected host: "{}" with hostid: {}'.format([e[1] for e in host_list if e[0] == int(select)].pop(), select))
+
+    items_list = get_items(int(select))
+
+    click.echo('Hostid\tItemid\tItemName')
+    for item in items_list:
+        click.echo('{}\t{}\t{}'.format(*item))
+    select = ''
+    items_ids = [e[1] for e in items_list]
+    while not select.isnumeric() or  int(select) not in items_ids:
+        select = click.prompt('\nSelect an itemid')
+
+        if not select.isnumeric() or int(select) not in items_ids:
+            click.echo('\nPlease use a number from the list')
+            print(items_ids)  
+
+    
+    item = [e for e in items_list if e[1] == int(select)].pop()
+    select = int(select)
+    click.echo('You have selected the item: {}'.format(item[2]))
+
+    # (trends.itemid, trends.value_min, trends.value_avg, trends.value_max)
+    trends = get_trends(select)
+    # print(select, type(select))
+    if trends == []:
+        click.echo('No trends in database for that item')
+        exit()
+
+    if click.confirm('Do you want to see it?'):
+        click.echo('Itemid\tMin\t\tAvg\t\tMax')
+        for trend in trends:
+            click.echo('{}\t{}\t\t{}\t\t{}'.format(*trend))
+
+    if click.confirm('Do you want to export it to a .csv file? \nThe file if exist will be deleted'):
+        file = ''
+        while file == '':
+            file = click.prompt('Name the file')
+            try:
+                fd = open(file,'w')
+            
+            except Exception as e:
+                click.echo('Error opening the file')
+                print(e)
+                print('try again')
+                file = ''
+            pass
+
+        click.echo('Itemid;Min;Avg;Max', file=fd)
+
+        for trend in trends:
+            click.echo('{};{};{};{}'.format(*trend), file=fd)
+
+	
+    numbers_max =  [e[3] for e in trends]
+    num_max = mean(numbers_max)
+    
+    numbers_avg =  [e[2] for e in trends]
+    num_avg= mean(numbers_avg)
+
+    numbers_min =  [e[1] for e in trends]
+    num_min = mean(numbers_min)
+
+    score_max = [e[3] for e in trends]
+    score_max.sort()
+    p_max = percentil(score_max)
+    ans_max = rank(score_max, p_max, 0.95)
+
+    score_avg = [e[2] for e in trends]
+    score_avg.sort()
+    p_avg = percentil(score_avg)
+    ans_avg = rank(score_avg, p_avg, 0.95)
+
+    score_min = [e[1] for e in trends]
+    score_min.sort()
+    p_min = percentil(score_min)
+    ans_min = rank(score_min, p_min, 0.95)
+
+    print('\nAverage max', num_max)
+    print('\n95 percentile max', ans_max)
+    print('\nAverage avg', num_avg)
+    print('\n95 percentile avg', ans_avg)
+    print('\nAverage min', num_min)
+    print('\n95 percentile min', ans_min)
+    print('\n')
+
+
+
+hosts()
