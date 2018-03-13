d81aecab0723145704e592e4dcc933269e93f24d
Lawrence Gil <larrywtf609@gmail.com>
2018-03-13 15:26:56 -0400

    #6Added support to filter by date and time
    It now prompts you if you wanna filter by date and time, it also checks if you have filtered the results, so it save the filtered results accordingly and it uses the filtered results to calculate the mean and 95 percentile values
    
    Now you don't have to manually type '.csv' when saving the file, it does it automatically

--- zabbixInfo.py
+++ zabbixInfo.py
@@ -1,5 +1,7 @@
 import click
-from readingdb import get_items, get_trends, search_host, percentil, rank, mean
+#to work with timestamps
+import datetime,time
+from readingdb import get_items, get_trends, search_host, percentil, rank, mean,filter_time
 
 @click.command()
 @click.option('--name', prompt='What host are you searching for?',help='Host to look for.')
@@ -41,9 +43,10 @@ def hosts(name):
 
     item = [e for e in items_list if e[1] == int(select)].pop()
     select = int(select)
+    itemid = select
     click.echo('You have selected the item: {}'.format(item[2]))
 
-    # (trends.itemid, trends.value_min, trends.value_avg, trends.value_max)
+    # (trends.itemid, trends.value_min, trends.value_avg, trends.value_max, trends.clock)
     trends = get_trends(select)
     # print(select, type(select))
     if trends == []:
@@ -51,14 +54,39 @@ def hosts(name):
         exit()
 
     if click.confirm('Do you want to see it?'):
-        click.echo('Itemid\tMin\t\tAvg\t\tMax')
         for trend in trends:
-            click.echo('{}\t{}\t\t{}\t\t{}'.format(*trend))
+            #to convert the timestamp into a human readable format
+            clock = trend[4]
+            date=datetime.datetime.fromtimestamp(int(clock)).strftime('%Y-%m-%d %H:%M:%S')
+            click.echo('{}\t{}\t\t{}\t\t{}\t\t{}'.format(trend[0],trend[1],trend[2],trend[3],date))
 
+    #Tells the rest of the program if the results will be filtered or not (is needed later)
+    isfiltered=False
+
+    if click.confirm('Do you want to filter by date and time?'):
+        date = click.prompt('Insert the date and time to start with\nIt must be in the format YYYYMMDDHHMMSS (all together for now)')
+        #fetchs the date from the 'date' string, so it can generate a Unix timestamp from it
+        datetuple = (int(date[0:4]),int(date[4:6]),int(date[6:8]),int(date[8:10]),int(date[10:12]),int(date[12:14]),0,0,0)
+        timestamp1 = int(time.mktime(datetuple))
+        date = click.prompt('\nInsert the date and time to finish the search\nIt must be in the format YYYYMMDDHHMMSS (all together for now)')        
+        datetuple = (int(date[0:4]),int(date[4:6]),int(date[6:8]),int(date[8:10]),int(date[10:12]),int(date[12:14]),0,0,0)
+        timestamp2 = int(time.mktime(datetuple))
+        filtered = filter_time(timestamp1,timestamp2,itemid)
+        #Sets the variable to True so the rest of the programs knows that it's working with filtered values
+        isfiltered = True
+
+        if click.confirm('Do you want to see it?'):
+            for sample in filtered:
+                clock = sample[4]
+                date=datetime.datetime.fromtimestamp(int(clock)).strftime('%Y-%m-%d %H:%M:%S')
+                click.echo('{}\t{}\t\t{}\t\t{}\t\t{}'.format(sample[0],sample[1],sample[2],sample[3],date))
+
+    
     if click.confirm('Do you want to export it to a .csv file? \nThe file if exist will be deleted'):
         file = ''
         while file == '':
-            file = click.prompt('Name the file')
+            userinput = click.prompt('Name the file')
+            file = str(userinput + '.csv')
             try:
                 fd = open(file, 'w')
 
@@ -70,33 +98,66 @@ def hosts(name):
             pass
 
         click.echo('Itemid;Min;Avg;Max', file=fd)
+        if isfiltered == False:
+            for trend in trends:
+                click.echo('{};{};{};{}'.format(*trend), file=fd)
 
-        for trend in trends:
-            click.echo('{};{};{};{}'.format(*trend), file=fd)
+        else:
+            for sample in filtered:
+                clock = sample[4]
+                date=datetime.datetime.fromtimestamp(int(clock)).strftime('%Y-%m-%d %H:%M:%S')
+                click.echo('{}\t{}\t\t{}\t\t{}\t\t{}'.format(sample[0],sample[1],sample[2],sample[3],date), file=fd)
 
-    numbers_max = [e[3] for e in trends]
-    num_max = mean(numbers_max)
+    if isfiltered == False:
+        numbers_max = [e[3] for e in trends]
+        num_max = mean(numbers_max)
 
-    numbers_avg = [e[2] for e in trends]
-    num_avg = mean(numbers_avg)
+        numbers_avg = [e[2] for e in trends]
+        num_avg = mean(numbers_avg)
 
-    numbers_min = [e[1] for e in trends]
-    num_min = mean(numbers_min)
+        numbers_min = [e[1] for e in trends]
+        num_min = mean(numbers_min)
 
-    score_max = [e[3] for e in trends]
-    score_max.sort()
-    p_max = percentil(score_max)
-    ans_max = rank(score_max, p_max, 0.95)
+        score_max = [e[3] for e in trends]
+        score_max.sort()
+        p_max = percentil(score_max)
+        ans_max = rank(score_max, p_max, 0.95)
 
-    score_avg = [e[2] for e in trends]
-    score_avg.sort()
-    p_avg = percentil(score_avg)
-    ans_avg = rank(score_avg, p_avg, 0.95)
+        score_avg = [e[2] for e in trends]
+        score_avg.sort()
+        p_avg = percentil(score_avg)
+        ans_avg = rank(score_avg, p_avg, 0.95)
 
-    score_min = [e[1] for e in trends]
-    score_min.sort()
-    p_min = percentil(score_min)
-    ans_min = rank(score_min, p_min, 0.95)
+        score_min = [e[1] for e in trends]
+        score_min.sort()
+        p_min = percentil(score_min)
+        ans_min = rank(score_min, p_min, 0.95)
+
+    else:
+        numbers_max = [sample[3] for sample in filtered]
+        num_max = mean(numbers_max)
+
+        numbers_avg = [sample[2] for sample in filtered]
+        num_avg = mean(numbers_avg)
+
+        numbers_min = [sample[1] for sample in filtered]
+        num_min = mean(numbers_min)
+
+        score_max = [sample[3] for sample in filtered]
+        score_max.sort()
+        p_max = percentil(score_max)
+        ans_max = rank(score_max, p_max, 0.95)
+
+        score_avg = [sample[2] for sample in filtered]
+        score_avg.sort()
+        p_avg = percentil(score_avg)
+        ans_avg = rank(score_avg, p_avg, 0.95)
+
+        score_min = [sample[1] for sample in filtered]
+        score_min.sort()
+        p_min = percentil(score_min)
+        ans_min = rank(score_min, p_min, 0.95)
+
 
     print('\nAverage max', num_max)
     print('\n95 percentile max', ans_max)
