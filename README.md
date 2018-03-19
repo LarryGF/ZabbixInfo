e298414312629bc554c1e434a993562e04bcd6da
LarryGF <larrywtf609@gmail.com>
2018-03-19 15:43:57 -0400

    saves the .csv int a csv folder, resolved the authentication problem

--- README.md
+++ README.md
@@ -9,11 +9,7 @@ So far it queries the **Zabbix** database in order to search for a specific host
 
 It lets you export the data of the selected item into a **.csv** file (optional) and gives you the mean of the maximum, minimum and average values that **Zabbix** has stored on its' database. 
 
-You can search the host you're looking for by passing the option **--name**, but if you don't it will still prompt you for one.
-
-It will ask you for the database's user, password and address (defaults to: **'root'**, **'1234'**, **'localhost'** respectively). So far you can't connect to a remote database, but we'll work on that.
-
-You can now filter the results by date and time.
+Since this is the very first version of the script you need to manually replace in your readingdb.py your database's username and password, the default **username**:**password** is **root**:**1234**. We're planning on doing this in a smarter way soon :)
 
 Installation
 -------------------
--- readingdb.py
+++ readingdb.py
@@ -3,103 +3,107 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import and_
+
 #Prompts the user to select the databse's user and password
-user=click.prompt("\nType your database's user",default='root')
-password =click.prompt("Type your database's password",hide_input=True,default='1234')
-dbaddr = click.prompt("Type your database's IP address",default='localhost')
 
 #This is the string that needs to be passed to the create_engine function (if anyone finds a better way to do this, feel free to edit)
-string='mysql://'+user+':'+password+'@'+dbaddr+'/zabbix' 
 
 #engine = create_engine('mysql://root:1234@localhost/zabbix', echo=False)
-engine = create_engine(string, echo=False) 
 
 #I know this is kinda basic, but it's a way to check if the user and password are correct
-noError = False
-count = int(0)
-while noError==False:
-    try:
-        Base = automap_base()
-        Base.prepare(engine, reflect=True)
-        Session = sessionmaker(bind=engine, expire_on_commit=False)
-        session = Session()
-        noError = True
-    except Exception as e:
-        count =count+1
+def db_connect():    
+	"""
+	Connects to the DB
+	"""
+	#I know these shouldn't be global variables, but that's the solution I came up with
+	global session,hosts,items,trends
+	noError = False
+	count = int(0)
+	while noError==False:
+		try:
+			user = click.prompt("\nType your database's user", default='root')
+			password =click.prompt("Type your database's password",hide_input=True,default='1234')
+			dbaddr = click.prompt("Type your database's IP address",default='localhost')
+			#string='mysql://'+user+':'+password+'@'+dbaddr+'/zabbix' 
+			string='mysql://{}:{}@{}/zabbix'.format(user, password, dbaddr)
+			#string='mysql://%s:%s@%s/zabbix' % (user, password, dbaddr)
+			engine = create_engine(string, echo=False) 
+			Base = automap_base()
+			Base.prepare(engine, reflect=True)
+			Session = sessionmaker(bind=engine, expire_on_commit=False)
+			session = Session()
+			noError = True
+			hosts = Base.classes['hosts']
+			items = Base.classes['items']
+			trends = Base.classes['trends']
 
-        if count < 3:
-            user=click.prompt("\nYou typed the wrong user or password, retype user",default='root')
-            password =click.prompt("Retype password",hide_input=True,default='1234')
-            dbaddr = click.prompt("Retype IP address",default='localhost')
-            
-        else:
-            print("\nIt seems like you don't know your database very well,exiting for your own sake...\n")
-            exit()            
-    pass
+		except Exception as e:
+			print('\n'+str(e))
+			# help(e)
+			count =count+1
 
-# File = Base.classes.files
-# a = session.query(File).filter(File.filename.like('%snake%')).all()
-# print([(e.path_parent + e.filename) for e in a])
-# ilike no es case sensitive
-hosts = Base.classes['hosts']
-items = Base.classes['items']
-trends = Base.classes['trends']
+			if count < 3:
+				pass
+			else:
+				print("\nIt seems like you don't know your database very well,exiting for your own sake...\n")
+				exit()            
+		pass
 
 
 def search_host(hostname: str):
-    """
-    El método recive un hostname y devuelve
+	"""
+	Receives a hostname and returns
 
-    (hosts.id, hosts.name)
+	(hosts.id, hosts.name)
 
-    donde hostname es un substring de hosts.name
-    """
-    return session.query(hosts.hostid, hosts.name).filter(hosts.name.ilike('%{}%'.format(hostname))).all()
+	where hostname is a substring of hosts.name
+	"""
+	return session.query(hosts.hostid, hosts.name).filter(hosts.name.ilike('%{}%'.format(hostname))).all()
 
 
 def get_items(hostid: int):
-    """
-    El método recive un hostid y devuelve
+	"""
+	Receives a hostid and returns
 
-    (items.hostid, items.itemid, items.name)
+	(items.hostid, items.itemid, items.name)
 
-    donde items.hostid == hostid
-    """
-    return session.query(items.hostid, items.itemid, items.name).filter(items.hostid == hostid).all()
+	where items.hostid == hostid
+	"""
+	return session.query(items.hostid, items.itemid, items.name).filter(items.hostid == hostid).all()
 
 
 def get_trends(itemid: int):
-    """
-    El método recive un itemid y devuelve
+	"""
+	Receives an itemid and returns
 
-    (trends.itemid, trends.value_min, trends.value_avg, trends.value_max, trends.clock)
+	(trends.itemid, trends.value_min, trends.value_avg, trends.value_max)
 
-    donde trends. itemid == hostid
-    """
-    return session.query(trends.itemid, trends.value_min, trends.value_avg, trends.value_max, trends.clock).filter(trends.itemid == itemid).all()
+	where trends. itemid == hostid
+	"""
+	return session.query(trends.itemid, trends.value_min, trends.value_avg, trends.value_max, trends.clock).filter(trends.itemid == itemid).all()
 
 
 def percentil(scores):
-    count = 0
-    ans = []
-    for i, score in enumerate(scores):
-        i += 1
-        ans.append(i / len(scores))
+	count = 0
+	ans = []
+	for i, score in enumerate(scores):
+		i += 1
+		ans.append(i / len(scores))
 
-    return ans
+	return ans
 
 
 def rank(scores, percentil_rank, value):
 
-    for i, e in enumerate(percentil_rank):
-        if e >= value:
-            return scores[i]
+	for i, e in enumerate(percentil_rank):
+		if e >= value:
+			return scores[i]
 
 
 def mean(numbers):
-    mean = float(sum(numbers)) / max(len(numbers), 1)
-    return mean
+	mean = float(sum(numbers)) / max(len(numbers), 1)
+	return mean
 
-#Filter the trends where timestamp1 <= time <= timestamp2 & itemid==hostid
+
 def filter_time(timestamp1,timestamp2,itemid: int):
-    return session.query(trends.itemid, trends.value_min, trends.value_avg, trends.value_max, trends.clock).filter(and_(trends.itemid == itemid,trends.clock >= timestamp1,trends.clock <= timestamp2)).all()
+	return session.query(trends.itemid, trends.value_min, trends.value_avg, trends.value_max, trends.clock).filter(and_(trends.itemid == itemid,trends.clock >= timestamp1,trends.clock <= timestamp2)).all()
